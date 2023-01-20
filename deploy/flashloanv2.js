d7759d6b6df5fb5789a7a839a09a2074272c9d56
guochijun <19802021353@139.com>
2023-01-20 16:29:44 +0800

    deposit

--- deploy/flashloanv2.js
+++ deploy/flashloanv2.js
@@ -5,9 +5,12 @@ const {
     VERIFICATION_BLOCK_CONFIRMATIONS,
 } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
+const IERC20_SOURCE = "@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20";
+const TOKEN_ADDRESS="0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60"
 module.exports = async ({ getNamedAccounts, deployments }) => {
+  const accounts=await ethers.getSigners()
+  const deployer=accounts[0]
   const { deploy, log } = deployments
-  const { deployer } = await getNamedAccounts()
   const chainId = network.config.chainId
   const waitBlockConfirmations = developmentChains.includes(network.name)? 1 : VERIFICATION_BLOCK_CONFIRMATIONS
   const arguments = [
@@ -15,19 +18,27 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   ]
   console.log(2222,arguments)
   const flashLoan = await deploy("flashloanv2", {
-    from: deployer,
+    from: deployer.address,
     args: arguments,
     log: true,
     waitConfirmations: waitBlockConfirmations,
   })
-    // Verify the deployment
-    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-      log("Verifying...")
-      await verify(flashLoan.address, arguments)
-    }
-  
-    // log("Enter flashLoan with command:")
-    // const networkName = network.name == "hardhat" ? "localhost" : network.name
-    // log("----------------------------------------------------")
+  console.log("deployed")
+  tokenContract = await ethers.getContractAt(IERC20_SOURCE, TOKEN_ADDRESS,  deployer.address);
+  console.log("tokenContract get")
+  tokenContract = tokenContract.connect(deployer);
+  console.log("tokenContract connected",tokenContract.address, deployer.address)
+  const balance=await tokenContract.balanceOf( deployer.address)
+  console.log("deployer token balance", balance.toString(),ethers.utils.parseEther("10").toString())
+  await tokenContract.transfer(tokenContract.address,ethers.utils.parseEther("10"));
+  // Verify the deployment
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...")
+    await verify(flashLoan.address, arguments)
+  }
+
+  // log("Enter flashLoan with command:")
+  // const networkName = network.name == "hardhat" ? "localhost" : network.name
+  // log("----------------------------------------------------")
 }
 module.exports.tags = ["all", "flashloanv2"]
--- scripts/flashloanv2.js
+++ scripts/flashloanv2.js
@@ -1,14 +1,16 @@
 const {network ,ethers,deployments,getNamedAccounts}=require("hardhat")
+const TOKEN_ADDRESS="0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60"
 async function startFlashLoan() {
-  accounts=await await ethers.getSigners()
-  deployer=accounts[0]
-  player=accounts[1]
+  const accounts=await ethers.getSigners()
+  const deployer=accounts[0]
   const flashLoanContract = await ethers.getContract("flashloanv2")
-  flashLoan = flashLoanContract.connect(deployer)
-  // console.log(network,flashLoanContract.address)
-  let data=await flashLoan.test()
-  // const entranceFee = await flashLoan.startFlashLoan("0xDF1742fE5b0bFc12331D8EAec6b478DfDbD31464",BigInt(1 * 10**18))
-  // console.log("Entered!")
+  const flashLoan = flashLoanContract.connect(deployer)
+  // console.log(network,flashLoanContract.address,flashLoan.address)
+  const lendPool=await flashLoan.LENDING_POOL()
+  let data=await flashLoan.getBalance(TOKEN_ADDRESS,lendPool)
+  let tx=await flashLoan.deposit2(TOKEN_ADDRESS)
+  // const entranceFee = await flashLoan.startFlashLoan("0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60",BigInt(1 * 10**18))
+  console.log("Entered!",tx)
   //
 }
 
